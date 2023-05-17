d5b74e59c1b7da95898edd5c98aac44f022ee843
Weiyi Kong <git@momok.cyou>
2023-05-17 20:58:02 +1000

    update: footer

---
+++ footer.php
@@ -0,0 +1,22 @@
+<?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
+
+                    <?php $this->need('partial/footer.php'); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="<?php $this->options->themeUrl('source/js/nanobar.min.js'); ?>"></script>
+    <script>
+        var options = {
+            classname: 'nanobar',
+            id: 'myNanobar'
+        };
+        var nanobar = new Nanobar(options);
+        nanobar.go(30);
+        nanobar.go(76);
+        nanobar.go(100);
+    </script>
+</body>
+
+</html>
