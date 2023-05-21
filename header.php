84e0a452061017970972b858d59221f456c3a3cc
Weiyi Kong <git@momok.cyou>
2023-05-21 21:32:05 +1000

    Replace local Prism with CDN link.

--- header.php
+++ header.php
@@ -57,8 +57,8 @@
     <link rel="stylesheet" href="<?php $this->options->themeUrl('source/css/normalize.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('source/css/skeleton.css'); ?>">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('source/css/minima.css'); ?>">
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('source/css/prism-dark.css'); ?>">
-    <link rel="stylesheet" href="<?php $this->options->themeUrl('source/css/prism-line-numbers.css'); ?>">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/themes/prism-okaidia.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/prismjs@1.29.0/plugins/line-numbers/prism-line-numbers.min.css">
     <link rel="stylesheet" href="<?php $this->options->themeUrl('source/css/custom.css'); ?>">
 
     <!-- Favicon -->
